337bf0e34e63664d58b9eaa81b516cb4c6a206d8
김성범 <love29ana@gmail.com>
2024-11-25 22:43:40 +0900

    Not Found Page

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./components/Root";
-import User from "./components/routes/Users";
 import Home from "./components/routes/Home";
+import NotFound from "./components/routes/NotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <NotFound />,
     children: [
       {
         path: "",
         element: <Home />,
-      },
-      {
-        path: "users",
-        element: <User />,
       },
     ],
   },
