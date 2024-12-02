bf5ae2c8eef93af526dc7339d1d3b9c219402924
김성범 <love29ana@gmail.com>
2024-12-02 23:16:45 +0900

    Rooms Grid

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -1,3 +1,48 @@
+import {
+  Box,
+  Grid,
+  Heading,
+  HStack,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { FaStar } from "react-icons/fa";
+
 export default function Home() {
-  return <span>Home</span>;
+  return (
+    <Grid
+      mt={10}
+      px={40}
+      columnGap={4}
+      rowGap={8}
+      templateColumns={"repeat(5, 1fr)"}
+    >
+      <VStack alignItems={"flex-start"}>
+        <Box overflow={"hidden"} mb={2} rounded={"3xl"}>
+          <Image
+            h="280"
+            src="https://a0.muscache.com/im/ml/photo_enhancement/pictures/e5d8daff-4593-4572-8ab1-9f3ceb325b0d.jpg?im_w=720"
+          />
+        </Box>
+        <Box>
+          <Grid gap={2} templateColumns={"6fr 1fr"}>
+            <Text as={"b"} fontSize={"md"} noOfLines={1}>
+              조천읍, 제주시, 한국의 타운하우스 전체
+            </Text>
+            <HStack spacing={1}>
+              <FaStar size={"15px"} />
+              <Text>5.0</Text>
+            </HStack>
+          </Grid>
+          <Text fontSize={"sm"} color={"gray.600"}>
+            Jeju, S. Korea
+          </Text>
+        </Box>
+        <Text fontSize={"sm"} color={"gray.600"}>
+          <Text as={"b"}>$72</Text> /night
+        </Text>
+      </VStack>
+    </Grid>
+  );
 }
